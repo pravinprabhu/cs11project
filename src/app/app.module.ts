acef6eb863a1ad5f443b4e34dfd6aca226a88e2d
pravin <pravinprabhu45@gmail.com>
2018-07-13 22:44:52 +0530

    ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {CheckboxModule} from 'primeng/checkbox';
+import { SignupComponent } from './signup/signup.component';
+import { AdminComponent } from './admin/admin.component';
+import { AppRoutingModule } from './app-routing.module';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import { StaffComponent } from './staff/staff.component';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignupComponent,
+    AdminComponent,
+    StaffComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    InputTextModule,
+    CheckboxModule,
+    AppRoutingModule,
+    RadioButtonModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
